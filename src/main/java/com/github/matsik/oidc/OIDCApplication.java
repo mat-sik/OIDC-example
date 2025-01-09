68379be6c3b88ffe02471c50013af43e24a70d2d
Mateusz <siiikor.sikorski@gmail.com>
2025-01-09 09:54:24 +0100

    Create simple project with OIDC support from spring with session store as redis

--- src/main/java/com/github/matsik/oidc/OIDCApplication.java
+++ src/main/java/com/github/matsik/oidc/OIDCApplication.java
@@ -1,22 +1,11 @@
 package com.github.matsik.oidc;
 
-import com.auth0.jwk.JwkProvider;
-import com.auth0.jwk.JwkProviderBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.matsik.oidc.google.GoogleOIDCEndpoints;
-import com.github.matsik.oidc.google.GoogleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.util.concurrent.TimeUnit;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -25,30 +14,6 @@ public class OIDCApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OIDCApplication.class, args);
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public JwkProvider jwkProvider(GoogleService googleService) throws MalformedURLException {
-        GoogleOIDCEndpoints endpoints = googleService.getEndpoints();
-
-        return new JwkProviderBuilder(URI.create(endpoints.jwksURI()).toURL())
-                .cached(10, 24, TimeUnit.HOURS)
-                .build();
-    }
-
-    @Bean
-    public CacheManager googleEndpointsCache() {
-        return new ConcurrentMapCacheManager("googleEndpointsCache");
     }
 
 }
---
+++ src/main/java/com/github/matsik/oidc/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.github.matsik.oidc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
